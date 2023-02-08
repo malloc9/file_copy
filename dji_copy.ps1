fad7eaba79c67533f3adafc59bde862dbd7d07ff
Attila Gyukics <mallocmalloc@gmail.com>
2023-02-08 21:17:56 +0100

    conditional start

--- dji_copy.ps1
+++ dji_copy.ps1
@@ -2,50 +2,53 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 $form = New-Object System.Windows.Forms.Form
-$form.Size = New-Object System.Drawing.Size(400,250)
-$form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
-$form.Text = "Copy files from J drive"
+$form.Size = New-Object System.Drawing.Size(500,200)
+$form.Text = "Copy Files"
 
-$label1 = New-Object System.Windows.Forms.Label
-$label1.Size = New-Object System.Drawing.Size(70, 20)
-$label1.Location = New-Object System.Drawing.Point(30, 30)
-$label1.Text = "Destination:"
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(30,20)
+$label.Size = New-Object System.Drawing.Size(200,20)
+$label.Text = "Destination Directory Name:"
+$form.Controls.Add($label)
 
-$textbox = New-Object System.Windows.Forms.TextBox
-$textbox.Size = New-Object System.Drawing.Size(200, 20)
-$textbox.Location = New-Object System.Drawing.Point(100, 30)
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(30,50)
+$textBox.Size = New-Object System.Drawing.Size(400,20)
+$form.Controls.Add($textBox)
 
-$copyButton = New-Object System.Windows.Forms.Button
-$copyButton.Size = New-Object System.Drawing.Size(80,20)
-$copyButton.Location = New-Object System.Drawing.Point(150, 60)
-$copyButton.Text = "Copy"
-$copyButton.Add_Click({
-    if ($textbox.Text -eq "") {
-        [System.Windows.Forms.MessageBox]::Show("Please enter destination name", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
-    } else {
-        $destination = "F:\Pictures\$((Get-Date).ToString('yyyy-MM-dd'))_$($textbox.Text)"
-        if (!(Test-Path $destination)) {
-            New-Item -ItemType Directory -Path $destination
+$progressBar = New-Object System.Windows.Forms.ProgressBar
+$progressBar.Location = New-Object System.Drawing.Point(30,90)
+$progressBar.Size = New-Object System.Drawing.Size(400,20)
+$progressBar.Minimum = 0
+$progressBar.Maximum = 100
+$form.Controls.Add($progressBar)
+
+$button = New-Object System.Windows.Forms.Button
+$button.Location = New-Object System.Drawing.Point(175,120)
+$button.Size = New-Object System.Drawing.Size(75,23)
+$button.Text = "Copy"
+$button.Add_Click({
+    $destinationDirectory = "F:\Pictures\" + (Get-Date -Format "yyyy-MM-dd") + "_" + $textBox.Text
+    If ($textBox.Text -ne "") {
+        If (Test-Path $destinationDirectory) {
+            Remove-Item -Path $destinationDirectory -Recurse
         }
+        New-Item -ItemType Directory -Path $destinationDirectory
+
         $files = Get-ChildItem "J:\" -File -Recurse
-        $progressBar = New-Object System.Windows.Forms.ProgressBar
-        $progressBar.Size = New-Object System.Drawing.Size(200,20)
-        $progressBar.Location = New-Object System.Drawing.Point(100, 100)
-        $progressBar.Minimum = 0
-        $progressBar.Maximum = $files.Count
-        $form.Controls.Add($progressBar)
-        $counter = 0
-        foreach ($file in $files) {
-            $counter++
-            $progressBar.Value = $counter
-            $destinationFile = Join-Path $destination $file.Name
-            Copy-Item $file.FullName $destinationFile
+        $count = $files.Count
+        $index = 0
+
+        ForEach ($file in $files) {
+            $progressBar.Value = [int](($index / $count) * 100)
+            $form.Refresh()
+            Copy-Item $file.FullName -Destination $destinationDirectory
+            $index++
         }
-        [System.Windows.Forms.MessageBox]::Show("Files copied successfully", "Info", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+        $progressBar.Value = 100
+        $form.Close()
     }
 })
+$form.Controls.Add($button)
 
-$form.Controls.Add($label1)
-$form.Controls.Add($textbox)
-$form.Controls.Add($copyButton)
 $form.ShowDialog()
